b4fac12b95515142d3629637c744d39412e0b8f4
rizasimkhana <riza.simkhanas@gmail.com>
2024-11-28 02:34:37 +0530

    images

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Product from './Product'
 function App() {
   // intalizing product variable
   const Products=[
-    {id:1,name:"Apples",image:"./dist/apples.png",description:"Fresh Apples" , price:30},
-    {id:2,name:"oranges",image:"./dist/oranges.png",description:"Fresh Oranges",price:40},
-    {id:3,name:"kiwi",image:"./dist//kiwi.png",description:"Fresh Kiwi",price:150},
-    {id:3,name:"pineapples",image:"./distpine.png",description:"Fresh pineapples",price:150},
-    {id:4,name:"watermelon",image:"./dist/watermelon.png",description:"Fresh watermelons",price:290},
-    {id:5,name:"pear",image:"./dist/pear.png",description:"Fresh pear",price:290},
+    {id:1,name:"Apples",image:"../images/apples.png",description:"Fresh Apples" , price:30},
+    {id:2,name:"oranges",image:"../images/oranges.png",description:"Fresh Oranges",price:40},
+    {id:3,name:"kiwi",image:"../images//kiwi.png",description:"Fresh Kiwi",price:150},
+    {id:3,name:"pineapples",image:"../images/pine.png",description:"Fresh pineapples",price:150},
+    {id:4,name:"watermelon",image:"../images/watermelon.png",description:"Fresh watermelons",price:290},
+    {id:5,name:"pear",image:"../images/pear.png",description:"Fresh pear",price:290},
    
   ]
 
